7b1c1ae2b3f4451f619c46760893a9455846d05f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 21:04:42 +0200

    ScoreBoard funciona

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider ot)
+    {
+        if(ot.gameObject.tag == "Player")
+        {
+            ScoreScript.scoreValue++;
+            LevelManager.CoinsRemaining++;
+            Destroy(this.gameObject);
+        }
+    }
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+}
